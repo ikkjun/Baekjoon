31604814e96858a437cfaff7a40e3e455e9203c9
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-01-03 19:11:11 +0900

    baekjoon

--- No_10869.java
+++ No_10869.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class No_10869 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
     }
 }
