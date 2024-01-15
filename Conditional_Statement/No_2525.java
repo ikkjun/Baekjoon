e280d27307ffb0bc401bbe16b26e5b0c67d56611
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-01-15 11:26:07 +0900

    Algorithm Study

--- Conditional_Statement/No_2525.java
+++ Conditional_Statement/No_2525.java
@@ -6,19 +6,21 @@ public class No_2525 {
     public static void main(String[] args) {
         Scanner scanner1 = new Scanner(System.in);
         Scanner scanner2 = new Scanner(System.in);
-        int hour = scanner1.nextInt();
-        int minute = scanner1.nextInt();
-        int cookingTime = scanner2.nextInt();
+        while(true) {
+            int hour = scanner1.nextInt();
+            int minute = scanner1.nextInt();
+            int cookingTime = scanner2.nextInt();
 
-        minute += cookingTime;
+            minute += cookingTime;
 
-        if(minute>=60) {
-            hour++;
-            if(hour>=24) {
-                hour = 0;
+            if(minute>=60) {
+                hour++;
+                if(hour>=24) {
+                    hour = 0;
+                }
+                minute -= 60;
             }
-            minute -= 60;
+            System.out.printf("%d %d", hour, minute);
         }
-        System.out.printf("%d %d", hour, minute);
     }
 }
