97df82d5c6993cf5320f19b9915103201f35f836
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-01-15 11:47:56 +0900

    Algorithm Study

--- Conditional_Statement/No_2525.java
+++ Conditional_Statement/No_2525.java
@@ -1,28 +1,47 @@
 package Conditional_Statement;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class No_2525 {
-    public static void main(String[] args) {
-        Scanner scanner1 = new Scanner(System.in);
-        Scanner scanner2 = new Scanner(System.in);
-        while(true) {
-            int hour = scanner1.nextInt();
-            int minute = scanner1.nextInt();
-            int cookingTime = scanner2.nextInt();
+    public static void main(String[] args) throws IOException {
+//        Scanner scanner = new Scanner(System.in);
+//        while(scanner.hasNextInt()) {
+//            int hour = scanner.nextInt();
+//            int minute = scanner.nextInt();
+//            int cookingTime = scanner.nextInt();
+//
+//            minute += cookingTime;
+//            while(minute>=60) {
+//                if(minute>=60) {
+//                    hour++;
+//                    if(hour>=24) {
+//                        hour = 0;
+//                    }
+//                    minute -= 60;
+//                }
+//            }
+//
+//            System.out.printf("%d %d%n", hour, minute);
+//        }
+//        scanner.close();
 
-            minute += cookingTime;
-            while(minute<60) {
-                if(minute>=60) {
-                    hour++;
-                    if(hour>=24) {
-                        hour = 0;
-                    }
-                    minute -= 60;
-                }
-            }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            System.out.printf("%d %d", hour, minute);
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int C = Integer.parseInt(br.readLine());
+
+        int min = (B+C) % 60;
+        int hou = (A + (B + C) / 60) % 24;
+
+        System.out.println(hou + " " + min);
+
     }
 }
