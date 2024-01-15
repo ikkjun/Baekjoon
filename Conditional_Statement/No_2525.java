502d0e271c5cd968a40961c64a5159355b7c335d
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-01-15 11:24:01 +0900

    Algorithm Study

---
+++ Conditional_Statement/No_2525.java
@@ -0,0 +1,24 @@
+package Conditional_Statement;
+
+import java.util.Scanner;
+
+public class No_2525 {
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+        int hour = scanner1.nextInt();
+        int minute = scanner1.nextInt();
+        int cookingTime = scanner2.nextInt();
+
+        minute += cookingTime;
+
+        if(minute>=60) {
+            hour++;
+            if(hour>=24) {
+                hour = 0;
+            }
+            minute -= 60;
+        }
+        System.out.printf("%d %d", hour, minute);
+    }
+}
